e767e4bb8e8fc8dc17bf46e819e802cabf179f01
Prerana Somarapu <psomarapu@ucmerced.edu>
2026-05-11 14:45:38 -0700

    comment

--- src/games/WordleFlowers.jsx
+++ src/games/WordleFlowers.jsx
@@ -3,6 +3,7 @@ import { useMemo, useState } from 'react';
 const WORDS = ['TULIP', 'ROSES', 'LILAC', 'DAISY', 'BLOOM', 'FERNS'];
 
 // hello
+//hello
 export default function WordleFlowers({ open, alreadySolved, onClose, onWin }) {
   const answer = useMemo(() => {
     const today = new Date();
